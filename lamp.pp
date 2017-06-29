6491d9d3025fd7ed87b878f64a7a555775815f13
root <root@puppet-client.ns.local>
2017-06-29 23:02:19 +0530

    initial commit

---
+++ lamp.pp
@@ -0,0 +1,28 @@
+exec {'apt-update':
+	command => '/usr/bin/apt-get update'
+}
+package {'apache2':
+	require => Exec ['apt-update'],
+	ensure => installed,
+}
+service {'apache2':
+	require => Package ['apache2'],
+	ensure => running,
+}
+package {'mysql-server':
+	require => Exec ['apt-update'],
+	ensure => installed,
+}
+service {'mysql':
+	require => Package ['mysql-server'],
+	ensure => running,
+}
+package {'php7.0':
+	require => Exec ['apt-update'],
+	ensure => installed,
+}
+file {'/var/www/html/php.info':
+	ensure => file,
+	content => '<?php phpinfo(); ?>'
+	require => Package ['apache2']
+}
